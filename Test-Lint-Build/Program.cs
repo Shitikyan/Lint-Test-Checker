b39698207b86ca3f074ba83fac2da6135972fb11
shitikyan <shitikyan@gmail.com>
2023-02-16 17:40:05 +0400

    Lint and Tets should fail

--- Test-Lint-Build/Program.cs
+++ Test-Lint-Build/Program.cs
@@ -12,7 +12,7 @@ internal class Program
         int x = 5;
         int y = 10;
         int sum = AddNumbers(x, y);
-        Console.WriteLine("The sum of {0} and {1} is {2}", x, y, sum);
+            Console.WriteLine("The sum of {0} and {1} is {2}", x, y, sum);
     }
 
     private static int AddNumbers(int a, int b)
